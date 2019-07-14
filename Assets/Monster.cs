b024e084948b4837353d94960d8654c617de0ea4
sailings <sailings@126.com>
2019-07-14 21:45:26 +0800

    1.完成丑小鸭踩死怪物功能 2.添加新的口水怪

--- Assets/Monster.cs
+++ Assets/Monster.cs
@@ -7,6 +7,21 @@ public class Monster : MonoBehaviour
     private float speed = -0.01f;
     public LayerMask TurnLayer;
 
+    private bool dead = false;
+    private bool playerDead = false;
+    private PlayerController controller;
+
+    private Vector3 effectPos1 = new Vector3(0.172f, 1.116f,0);
+    private Vector3 effectPos2 = new Vector3(-0.938f, 0.846f,0);
+
+    public GameObject KillByPlayerEffect;
+    public GameObject KillPlayerEffect;
+
+    private void Awake()
+    {
+        controller = FindObjectOfType<PlayerController>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +37,38 @@ public class Monster : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && !playerDead)
+        {
+            dead = true;
+            //Debug.Log("Monster Dead");
+            GameManager.Instance.Score += 100;
+
+            var effect = Instantiate(KillByPlayerEffect, controller.gameObject.transform);
+            effect.transform.localPosition = effectPos1;
+
+            gameObject.transform.localScale = new Vector3(1.0f,0.3f,1.0f);
+            controller.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            controller.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,300));
+
+            StartCoroutine(KillMonster(1.0f));
+        }
+    }
+
+    IEnumerator KillMonster(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        Destroy(gameObject);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && !dead)
         {
+            playerDead = true;
+            var effect = Instantiate(KillPlayerEffect, controller.gameObject.transform);
+            effect.transform.localPosition = effectPos2;
             GameManager.Instance.GameOver();
         }
     }
