e6e4c7a1d61450f8522a4272a8c1b4250efe58fa
sailings <sailings@126.com>
2019-07-14 21:15:59 +0800

    1.完成相机跟随 2.完成口水怪的动画制作 3.完成丑小鸭与口水怪的碰撞

---
+++ Assets/Monster.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    private float speed = -0.01f;
+    public LayerMask TurnLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(new Vector2(speed, 0));
+        if (Physics2D.Raycast(gameObject.transform.position, Vector2.left, 0.1f, TurnLayer))
+        {
+            transform.Rotate(Vector3.up, 180);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameManager.Instance.GameOver();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
