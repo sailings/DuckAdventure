88335ec0afd9d8d4a60cfb9b1b30ba009d68a9aa
sailings <sailings@126.com>
2019-07-16 23:27:46 +0800

    完成食人鱼的制作

---
+++ Assets/Script/Fish.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    private bool dead = false;
+    private bool playerDead = false;
+    private PlayerController controller;
+
+    private void Awake()
+    {
+        controller = FindObjectOfType<PlayerController>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Attack()
+    {
+        GetComponent<Rigidbody2D>().isKinematic = false;
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0,300));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && !playerDead)
+        {
+            dead = true;
+            GameManager.Instance.Score += 100;
+            //gameObject.transform.localScale = new Vector3(1.0f, 0.3f, 1.0f);
+            controller.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            controller.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 300));
+
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && !dead)
+        {
+            playerDead = true;
+            GameManager.Instance.GameOver();
+        }
+    }
+}
---
+++ Assets/Script/FishDetect.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FishDetect : MonoBehaviour
+{
+    public Fish fish;
+    public GameObject AttackPoint;
+    public float AttackDelay = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            fish.transform.DOMove(AttackPoint.transform.position, AttackDelay);
+            StartCoroutine(BeginAttack());
+        }
+    }
+
+    IEnumerator BeginAttack()
+    {
+        yield return new WaitForSeconds(AttackDelay);
+        fish.Attack();
+    }
+}
