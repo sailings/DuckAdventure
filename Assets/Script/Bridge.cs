bb8f0526e187e2143638200fa20c057f98329e95
sailings <sailings@126.com>
2019-07-20 12:56:04 +0800

    1.增加空中栈道 2.增加独木桥

---
+++ Assets/Script/Bridge.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rig;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator Fall(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        rig.isKinematic = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            animator.SetTrigger("Rotate");
+            //rig.isKinematic = false;
+            StartCoroutine(Fall(0.5f));
+            Destroy(gameObject, 3.0f);
+        }
+    }
+}
