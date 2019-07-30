25e5f3ca5fbb0aa31f3ed6e8999e1a8040c629bb
sailings <sailings@126.com>
2019-07-30 22:08:04 +0800

    添加Boss无敌技能，添加无敌，击中，死亡等粒子特效

--- Assets/Script/Boss.cs
+++ Assets/Script/Boss.cs
@@ -11,17 +11,50 @@ public class Boss : MonoBehaviour
     public GameObject[] DropMonsters;
     public float TotalLife = 100;
     private float lifeLeft;
+    private bool isProtected = false;
+    public GameObject[] ProtectEffect;
+
+    public GameObject DeadEffect;
+    public GameObject HitEffect;
+    public GameObject BulletMissEffect;
 
     private void Awake()
     {
         lifeLeft = TotalLife;
         controller = FindObjectOfType<PlayerController>();
+        for (int i = 0; i < ProtectEffect.Length; i++)
+        {
+            ProtectEffect[i].SetActive(false);
+        }
+    }
+
+    IEnumerator ProtectBoss()
+    {
+        while (!isDead)
+        {
+            yield return new WaitForSeconds(Random.Range(1.0f,2.0f));
+            isProtected = true;
+            controller = FindObjectOfType<PlayerController>();
+            for (int i = 0; i < ProtectEffect.Length; i++)
+            {
+                ProtectEffect[i].SetActive(false);
+            }
+            ProtectEffect[Random.Range(0, ProtectEffect.Length)].SetActive(true);
+            yield return new WaitForSeconds(Random.Range(2.0f, 4.0f));
+            isProtected = false;
+            controller = FindObjectOfType<PlayerController>();
+            for (int i = 0; i < ProtectEffect.Length; i++)
+            {
+                ProtectEffect[i].SetActive(false);
+            }
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
         BeginAttack();
+        StartCoroutine(ProtectBoss());
     }
 
     IEnumerator Attack()
@@ -47,12 +80,25 @@ public class Boss : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Bullet"))
         {
-            Debug.Log("Boss Hit");
-            lifeLeft -= 10;
-            if (lifeLeft <= 0)
+            if (!isProtected)
             {
-                Debug.Log("Boss Dead");
-                Destroy(gameObject);
+                Debug.Log("Boss Hit");
+                lifeLeft -= 10;
+                var hitEffect = Instantiate(HitEffect, collision.gameObject.transform.position, Quaternion.identity);
+                hitEffect.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
+                if (lifeLeft <= 0)
+                {
+                    Debug.Log("Boss Dead");
+                    isDead = true;
+                    GameManager.Instance.Score += 500;
+                    Instantiate(DeadEffect, gameObject.transform.position, Quaternion.identity);
+                    Destroy(gameObject);
+                }
+            }
+            else {
+                Debug.Log("Boss Protect");
+                var missEffect = Instantiate(BulletMissEffect,collision.gameObject.transform.position,Quaternion.identity);
+                missEffect.transform.position = collision.gameObject.transform.position - new Vector3(1, 0, 0);
             }
             Destroy(collision.gameObject);
         }
