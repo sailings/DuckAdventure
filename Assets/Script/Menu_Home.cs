eb4b19b2121698d2a5447e7b09ea50b18e8d88c5
sailings <sailings@126.com>
2019-08-17 22:27:00 +0800

    完成主菜单的状态切换，地图选择

---
+++ Assets/Script/Menu_Home.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_Home : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject WorldChoose;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenu.SetActive(true);
+        WorldChoose.SetActive(false);
+    }
+
+    public void Play()
+    {
+        MainMenu.SetActive(false);
+        WorldChoose.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        WorldChoose.SetActive(false);
+        MainMenu.SetActive(true);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
