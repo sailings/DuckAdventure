d3c16d31d2e31ee31d0e66087b3ac4571c3d0ae6
sailings <sailings@126.com>
2019-08-18 15:11:33 +0800

    完成关卡列表左右滑动功能

---
+++ Assets/WorldSlide.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class WorldSlide : MonoBehaviour
+{
+    private float posx;
+
+    public RectTransform Levels;
+    private float step = 450;
+    private float minX;
+    private float maxX;
+    private float blockCount = 2;
+    private float speed = 0.8f;
+
+    public GameObject WorldChoose;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posx = Levels.anchoredPosition.x;
+        maxX = posx;
+        minX = maxX - (blockCount - 1) * step;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pre()
+    {
+        posx += step;
+        posx = Mathf.Clamp(posx, minX, maxX);
+        //Levels.anchoredPosition = new Vector2(posx, Levels.anchoredPosition.y);
+        Levels.DOAnchorPosX(posx, speed);
+    }
+
+    public void Next()
+    {
+        posx -= step;
+        posx = Mathf.Clamp(posx, minX, maxX);
+        //Levels.anchoredPosition = new Vector2(posx, Levels.anchoredPosition.y);
+        Levels.DOAnchorPosX(posx, speed);
+    }
+
+    public void Close()
+    {
+        WorldChoose.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
