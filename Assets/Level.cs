ad26baffd8565595fbc44fbdfd58d762e2d3d618
sailings <sailings@126.com>
2019-08-18 14:01:36 +0800

    完成关卡列表展示

---
+++ Assets/Level.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Level : MonoBehaviour
+{
+    public int levelNum = 1;
+    public Text LevelText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int.TryParse(gameObject.name, out levelNum);
+        LevelText.text = levelNum.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
