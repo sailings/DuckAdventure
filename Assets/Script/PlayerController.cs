da3a38c78b37f3c2eb763d28a744ba606b66d2c2
sailings <sailings@126.com>
2019-09-16 22:17:56 +0800

    完成关卡W1-4

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -101,6 +101,7 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Saw"))
         {
+            SoundManager.Instance.PlaySound(SoundHitEnemy);
             Instantiate(BloodEffect, transform);
             GameManager.Instance.GameOver();
         }
