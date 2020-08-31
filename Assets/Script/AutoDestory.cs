ac04d15c31585c3e0ca8a40f49d0c00377030ee1
武潇 <wuxiao0063@163.com>
2020-08-31 15:07:20 +0800

    销毁时间再次修改

--- Assets/Script/AutoDestory.cs
+++ Assets/Script/AutoDestory.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AutoDestory : MonoBehaviour
 {
-    public float time = 2.0f;
+    public float time = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
